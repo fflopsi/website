44b58fb5f0d7b0f7447a135b8a20f6680d661d8b
fflopsi <florian.l.frauenfelder@gmail.com>
2025-03-19 19:02:26 +0100

    Use source code pro font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Source_Code_Pro } from "next/font/google";
 import "./var.css";
 import "./globals.css";
 import "./navBar.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const sourceCodePro = Source_Code_Pro({
   subsets: ["latin"],
 });
 
@@ -26,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${sourceCodePro.className} antialiased`}>
         {children}
       </body>
     </html>
