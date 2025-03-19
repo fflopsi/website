87ae06fed5572b91e20a9ea8afc7b75a0f0910df
fflopsi <florian.l.frauenfelder@gmail.com>
2025-03-20 00:34:28 +0100

    Use date-footer and title script

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Script from "next/script";
+import DateFooter from "@/public/js/dateFooter";
 
 export default function Home() {
   return (
     <div>
       <Script src="/js/navBar.js"/>
+      <Script src="/js/getTitle.js"/>
 
       <h1 id="title">Florian</h1>
 
@@ -30,7 +32,7 @@ export default function Home() {
 
       </div>
 
-      {/* <date-footer date="2025-03-11"></date-footer> */}
+      {DateFooter("2025-03-11")}
     </div>
   );
 }
