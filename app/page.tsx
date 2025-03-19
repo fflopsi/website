44a7442539e186d519a2a87b46a1f96909ee0fc2
fflopsi <florian.l.frauenfelder@gmail.com>
2025-03-19 11:47:32 +0100

    Add old home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,102 +2,32 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div>
+      <h1 id="title">Florian</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <nav active="/"><a>Home</a></nav>
+
+      <div id="content">
+
+      <h2>About me</h2>
+      <p>Hey there, lone traveler! How did you get here?</p>
+      <p>Some things about me I think may be worth knowing (not in any particular order):</p>
+        <ul>
+          <li>I was born in 2002 and grew up in a small town called <a target="_blank" href="https://en.wikipedia.org/wiki/B%C3%BClach">Bülach</a> in northern Switzerland near Zürich.</li>
+          <li>I have a motorcycle, a white <a target="_blank" href="https://en.wikipedia.org/wiki/KTM_390_series">KTM 390</a> Duke 2022, to be exact.</li>
+          <li>I study physics at <a target="_blank" href="https://ethz.ch"><abbr title="Eidgenössische Technische Hochschule Zürich">ETHZ</abbr></a>, currently in the 4<sup>th</sup> semester BSc.</li>
+          <li>I work as a <abbr title="Teaching Assistant">TA</abbr> at ETH, so far <a href="/ta/informatik/">for <q>Informatik</q> (for D-MATH, D-PHYS)</a> and <a href="/ta/linalg/"><q>Lineare Algebra II</q> (for D-MATH, D-PHYS)</a>. You can find all the material <a href="/ta/">here</a>.</li>
+          <li>I am a member and <q><abbr title="tour guide">demonstrator</abbr></q> of the observatory in Bülach (<a target="_blank" href="https://sternwartebuelach.ch">Schul- und Volkssternwarte Bülach</a>) and I sometimes do tours (public and private). Some of the material I've created is available <a href="/astro/">here</a>.</li>
+          <li>I have been teaching myself how to code since about 2016. If you are interested in what I have created so far, <a href="/coding/">have a look</a>.</li>
+        </ul>
+      <h2>Contact</h2>
+        <ul>
+          <li><a href="mailto:ffrauenfelde@student.ethz.ch">ffrauenfelde@student.ethz.ch</a></li>
+        </ul>
+
+      </div>
+
+      {/* <date-footer date="2025-03-11"></date-footer> */}
     </div>
   );
 }
