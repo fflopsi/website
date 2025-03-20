49328cdfbade063c34ddf7d76fe600e0bf0a728c
fflopsi <florian.l.frauenfelder@gmail.com>
2025-03-21 00:55:44 +0100

    Rewrite navbar

--- public/js/navBar.js
+++ public/js/navBar.js
@@ -1,28 +1,3 @@
-// Fill the nav tag with navigation links
-const nav = document.querySelector("nav");
-const active = nav.getAttribute("data-active") || "";
-fetch("/html/navBar.html")
-  .then((response) => response.text())
-  .then((html) => {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, "text/html");
-    const links = doc.querySelectorAll("a"); // Select all the links in the navBar
-
-    links.forEach((link) => {
-      const href = link.getAttribute("href");
-      if (
-        href &&
-        (active == href || (active.includes(href) && href.length >= 3))
-      ) {
-        link.classList.add("active");
-      }
-    });
-    nav.innerHTML = doc.body.innerHTML;
-  })
-  .catch((error) => {
-    console.error("Error loading navigation bar: ", error);
-  });
-
 // Add sticky class to navigation bar when on top of screen
 window.addEventListener("scroll", function () {
   const nav = document.querySelector("nav");
