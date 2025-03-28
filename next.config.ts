8bd02002721a29ee4058bac9747848a8d4cd1061
fflopsi <florian.l.frauenfelder@gmail.com>
2025-03-28 10:25:30 +0100

    Add redirects to /legal/

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   trailingSlash: true,
+  redirects: async () => [
+    {
+      source: '/(contact|copyright|disclaimer|privacy|impressum)/',
+      destination: '/legal/',
+      permanent: true,
+    },
+  ],
 };
 
 export default nextConfig;
