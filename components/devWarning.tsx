1e1d5bfe6d9de9a858d2cb7290505434ab3ab488
fflopsi <florian.l.frauenfelder@gmail.com>
2025-03-28 23:04:03 +0100

    Fix production link

--- components/devWarning.tsx
+++ components/devWarning.tsx
@@ -7,7 +7,7 @@ export default function DevWarning() {
         THIS IS A PREVIEW/DEVELOPMENT VERSION OF THIS WEBSITE!
       </h2>
       <p style={{ fontSize: '1.5rem', fontStyle: 'italic' }}>
-        I do not guarantee that anything on this version works as expected. If you want to see the normal, functional version, <Link style={{ color: 'red' }} href=''>click here</Link>.
+        I do not guarantee that anything on this version works as expected. If you want to see the normal, functional version, <Link style={{ color: 'red' }} href={`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`}>click here</Link>.
       </p>
       <hr />
     </>
