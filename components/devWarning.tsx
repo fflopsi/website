7dc41c263637661ba0ffc0c4a26d70a512b89dc4
fflopsi <florian.l.frauenfelder@gmail.com>
2025-03-28 23:05:07 +0100

    Add commit link

--- components/devWarning.tsx
+++ components/devWarning.tsx
@@ -9,6 +9,7 @@ export default function DevWarning() {
       <p style={{ fontSize: '1.5rem', fontStyle: 'italic' }}>
         I do not guarantee that anything on this version works as expected. If you want to see the normal, functional version, <Link style={{ color: 'red' }} href={`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`}>click here</Link>.
       </p>
+      <p>The present version of this website is based on commit <Link href={`https://${process.env.VERCEL_GIT_PROVIDER}.com/${process.env.VERCEL_GIT_REPO_OWNER}/${process.env.VERCEL_GIT_REPO_SLUG}/commit/${process.env.VERCEL_GIT_COMMIT_SHA}`}>{process.env.VERCEL_GIT_COMMIT_SHA}</Link>.</p>
       <hr />
     </>
   ) : '';
