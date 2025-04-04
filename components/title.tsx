1b0cd5ba77053697c4b3969f08ec9a44adb440b6
fflopsi <florian.l.frauenfelder@gmail.com>
2025-04-04 23:44:18 +0200

    Add suspense to client-side title fetching

--- components/title.tsx
+++ components/title.tsx
@@ -2,23 +2,26 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { Suspense } from 'react';
 import { getAttr } from './routing';
 
 export default function Title() {
   const title = getAttr(usePathname(), 'title');
   const titles = title.match(/^Florian(\b.*)/);
   return (
-    <h1>
-      {titles ? (
-        <>
-          <Link href='/' className='normal'>
-            Florian
-          </Link>
-          {titles[1]}
-        </>
-      ) : (
-        title
-      )}
-    </h1>
+    <Suspense fallback={<h1>Florian</h1>}>
+      <h1>
+        {titles ? (
+          <>
+            <Link href='/' className='normal'>
+              Florian
+            </Link>
+            {titles[1]}
+          </>
+        ) : (
+          title
+        )}
+      </h1>
+    </Suspense>
   );
 }
