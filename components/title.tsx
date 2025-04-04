ff86bea42b78aca629e21056d4067ed0d9a0d026
fflopsi <florian.l.frauenfelder@gmail.com>
2025-04-04 23:47:23 +0200

    Remove suspense

--- components/title.tsx
+++ components/title.tsx
@@ -2,26 +2,23 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { Suspense } from 'react';
 import { getAttr } from './routing';
 
 export default function Title() {
   const title = getAttr(usePathname(), 'title');
   const titles = title.match(/^Florian(\b.*)/);
   return (
-    <Suspense fallback={<h1>Florian</h1>}>
-      <h1>
-        {titles ? (
-          <>
-            <Link href='/' className='normal'>
-              Florian
-            </Link>
-            {titles[1]}
-          </>
-        ) : (
-          title
-        )}
-      </h1>
-    </Suspense>
+    <h1>
+      {titles ? (
+        <>
+          <Link href='/' className='normal'>
+            Florian
+          </Link>
+          {titles[1]}
+        </>
+      ) : (
+        title
+      )}
+    </h1>
   );
 }
