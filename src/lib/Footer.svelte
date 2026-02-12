a8338226befccc94fe173de642909cd16c4e561f
fflopsi <florian.l.frauenfelder@gmail.com>
2026-02-12 13:39:39 +0100

    Update copyright notice year

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,7 +2,7 @@
 <hr />
 <div style="margin-top: 4rem;"></div>
 <footer>
-  &copy;&nbsp;2025 Florian Frauenfelder // <a href="/">Home</a> //
+  &copy;&nbsp;2025&ndash;2026 Florian Frauenfelder // <a href="/">Home</a> //
   <a href="/contact/">Contact form</a>
   //
   <a href="/legal/">Legal: Contact, Copyright, Disclaimer, Privacy policy</a>
